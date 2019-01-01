626d801a8df790e61f9f8a9a8edc712962c4320a
Matthew Brown <github@muglug.com>
2019-01-01 12:17:18 -0500

    Get results on load

--- includes/script.php
+++ includes/script.php
@@ -24,42 +24,48 @@ var results = CodeMirror.fromTextArea(document.getElementById("php_code"), {
     readOnly: true
 });
 
+function refreshResults() {
+    var code = editor.getDoc().getValue();
+    latestFetch++;
+    fetchKey = latestFetch;
+    fetch('/convert.php', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+        },
+        body: serializeJSON({code: code})
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+        if (latestFetch != fetchKey) {
+            return;
+        }
+
+        if ('code' in response) {
+            results.getDoc().setValue(response.code);
+        }
+        else if ('error' in response) {
+            var hacktophp_header = 'Error output: \n\n'
+
+            results.getDoc().setValue(hacktophp_header + response.error);
+        }
+    })
+    .catch (function (error) {
+        console.log('Request failed', error);
+    });
+}
+
 editor.on(
     'change',
     function () {
-        var code = editor.getDoc().getValue();
-        latestFetch++;
-        fetchKey = latestFetch;
-        fetch('/convert.php', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-            },
-            body: serializeJSON({code: code})
-        })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            if (latestFetch != fetchKey) {
-                return;
-            }
-
-            if ('code' in response) {
-                results.getDoc().setValue(response.code);
-            }
-            else if ('error' in response) {
-                var hacktophp_header = 'Error output: \n\n'
-
-                results.getDoc().setValue(hacktophp_header + response.error);
-            }
-        })
-        .catch (function (error) {
-            console.log('Request failed', error);
-        });
+        refreshResults();
     }
 );
+
+refreshResults();
 
 //editor.focus();
 editor.setCursor(editor.lineCount(), 0);
